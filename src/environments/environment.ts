5055514eed9028b77bf94579521737ed199dc611
Patricia <patriciasteffen@hotmail.com>
2021-11-06 19:23:45 +0100

    firebase 8

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAlYDJM6hEXu4cub50lbPU5HHZPthPlIXc",
+    authDomain: "ski-pap.firebaseapp.com",
+    projectId: "ski-pap",
+    storageBucket: "ski-pap.appspot.com",
+    messagingSenderId: "808378301075",
+    appId: "1:808378301075:web:83ec21a509eb63d8aaab66",
+    measurementId: "G-JPM0RC4L9P"
+  }
+
 };
 
 /*
